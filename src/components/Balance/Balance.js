f1b1d46feee598c51e31e131a5a25f5b07b86bbe
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-16 22:59:57 +0200

    refactoring

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,85 +1,84 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import s from './Balance.module.css';
-import Notiflix from 'notiflix';
-import { authOperations, authSelectors } from '../../redux/auth';
-import Notification from '../../components/Notification/Notification';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Notiflix from "notiflix";
+import s from "./Balance.module.css";
+import { authOperations, authSelectors } from "../../redux/auth";
+import Notification from "../../components/Notification/Notification";
 
 const Balance = ({ hide, width }) => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
+    const balance = useSelector(authSelectors.getUserBalance);
+    const [sum, setSum] = useState(null);
+    const [setPromptClose, setClosePrompt] = useState(true);
 
-  const balance = useSelector(authSelectors.getUserBalance);
+    const toggleWindow = () => {
+        setClosePrompt((setClosePrompt) => !setClosePrompt);
+    };
 
-  const [sum, setSum] = useState(null);
+    const onHandleChange = (e) => setSum(e.currentTarget.value);
 
-  const [setPromptClose, setClosePrompt] = useState(true);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (sum === null) {
+            Notiflix.Notify.info("You should earn some money and come back))", {
+                timeout: 1500,
+            });
+            return;
+        }
 
-  const toggleWindow = () => {
-    setClosePrompt(setClosePrompt => !setClosePrompt);
-  };
-
-  const onHandleChange = e => setSum(e.currentTarget.value);
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (sum === null) {
-      Notiflix.Notify.info('You should earn some money and come back))', {
-        timeout: 1500,
-      });
-      return;
-    }
-
-    dispatch(authOperations.updateBalance({ balance: +sum }));
-  };
-  return (
-    <form onSubmit={handleSubmit} className={s.reportBalance}>
-      <label htmlFor="balance" className={s.balanceLabel}>
-        Balance:
-        <div className={s.buttonsGroup}>
-          {balance === 0 || balance === null ? (
-            <>
-              {setPromptClose && <Notification onClose={toggleWindow} />}
-              <input
-                type="text"
-                name="name"
-                maxLength="10"
-                placeholder="00.00"
-                onChange={onHandleChange}
-                className={
-                  width
-                    ? `${s.balanceInputReport} ${s.balanceInput}`
-                    : `${s.balanceInput}`
-                }
-                autoComplete="off"
-                // id="balance"
-              />
-              <button
-                className={
-                  width
-                    ? `${s.balanceInputReport} ${s.balanceButton}`
-                    : `${s.balanceButton} `
-                }
-                type="submit"
-              >
-                CONFIRM
-              </button>
-            </>
-          ) : (
-            <>
-              <p
-                className={
-                  width
-                    ? `${s.balanceInput} ${s.balanceInputReport}`
-                    : `${s.balanceInput}`
-                }
-              >
-                {balance} UAH
-              </p>
-            </>
-          )}
-        </div>
-      </label>
-    </form>
-  );
+        dispatch(authOperations.updateBalance({ balance: +sum }));
+    };
+    return (
+        <form onSubmit={handleSubmit} className={s.reportBalance}>
+            <label htmlFor="balance" className={s.balanceLabel}>
+                Balance:
+                <div className={s.buttonsGroup}>
+                    {balance === 0 || balance === null ? (
+                        <>
+                            {setPromptClose && (
+                                <Notification onClose={toggleWindow} />
+                            )}
+                            <input
+                                type="text"
+                                name="name"
+                                maxLength="10"
+                                placeholder="00.00"
+                                onChange={onHandleChange}
+                                className={
+                                    width
+                                        ? `${s.balanceInputReport} ${s.balanceInput}`
+                                        : `${s.balanceInput}`
+                                }
+                                autoComplete="off"
+                                // id="balance"
+                            />
+                            <button
+                                className={
+                                    width
+                                        ? `${s.balanceInputReport} ${s.balanceButton}`
+                                        : `${s.balanceButton} `
+                                }
+                                type="submit"
+                            >
+                                CONFIRM
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <p
+                                className={
+                                    width
+                                        ? `${s.balanceInput} ${s.balanceInputReport}`
+                                        : `${s.balanceInput}`
+                                }
+                            >
+                                {balance} UAH
+                            </p>
+                        </>
+                    )}
+                </div>
+            </label>
+        </form>
+    );
 };
 export default Balance;
