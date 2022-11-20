b4429f6473065044bbe3f0459c43d4b426d23301
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-20 14:35:07 +0200

    refactoring

--- src/components/LogoutButton/LogoutButton.jsx
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -1,33 +1,34 @@
-import s from './LogoutButton.module.css';
-import Modal from '../../components/Modal/Modal';
-import { useDispatch } from 'react-redux';
-import { useState } from 'react';
-import { authOperations } from '../../redux/auth';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { authOperations } from "../../redux/auth";
+import Modal from "../../components/Modal/Modal";
+import s from "./LogoutButton.module.css";
+
 const LogoutButton = () => {
-  const [showModal, setShowModal] = useState(false);
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <button
-        type="button"
-        className={s.btn}
-        onClick={() => setShowModal(true)}
-      >
-        {/* <svg className="icon">
+    const [showModal, setShowModal] = useState(false);
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <button
+                type="button"
+                className={s.btn}
+                onClick={() => setShowModal(true)}
+            >
+                {/* <svg className="icon">
           <use href="./sprite.svg#icon-logout"></use>
         </svg> */}
-        Exit
-      </button>
-      {showModal ? (
-        <Modal
-          onClick={() => dispatch(authOperations.logOut())}
-          setShowModal={setShowModal}
-        >
-          Do you really want to leave?
-        </Modal>
-      ) : null}
-    </div>
-  );
+                Exit
+            </button>
+            {showModal ? (
+                <Modal
+                    onClick={() => dispatch(authOperations.logOut())}
+                    setShowModal={setShowModal}
+                >
+                    Do you really want to leave?
+                </Modal>
+            ) : null}
+        </div>
+    );
 };
 
 export default LogoutButton;
