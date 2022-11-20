6b7e9d5f7845f59d335bc1d4b70ebd0ea29ca0ba
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-20 14:34:35 +0200

    refactoring

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,38 +1,41 @@
-import { useFetchSummaryQuery } from '../../redux/transactions/transactionsApi';
-<<<<<<< Updated upstream
-import months from '../../data/summary.json';
-=======
-import months from '../../data/months.json';
->>>>>>> Stashed changes
-import s from './Summary.module.css';
+import { useFetchSummaryQuery } from "../../redux/transactions/transactionsApi";
+import months from "../../data/months.json";
+import s from "./Summary.module.css";
 
-const Summary = ({ type = 'expense' }) => {
-  const { data } = useFetchSummaryQuery(type);
+const Summary = ({ type = "expense" }) => {
+    const { data } = useFetchSummaryQuery(type);
 
-  return (
-    <div className={s.container}>
-      <p className={s.title}>Summary</p>
-      <table className={s.list}>
-        <>
-          <tbody>
-            {data?.transactions &&
-              [...data.transactions]
-                .sort((a, b) => b._id.month - a._id.month)
-                .map(({ _id, total }, index) => {
-                  return (
-                    index < 6 && (
-                      <tr className={s.item} key={_id.month}>
-                        <td className={s.description}>{months[_id.month]}</td>
-                        <td className={s.description}>{total}</td>
-                      </tr>
-                    )
-                  );
-                })}
-          </tbody>
-        </>
-      </table>
-    </div>
-  );
+    return (
+        <div className={s.container}>
+            <p className={s.title}>Summary</p>
+            <table className={s.list}>
+                <>
+                    <tbody>
+                        {data?.transactions &&
+                            [...data.transactions]
+                                .sort((a, b) => b._id.month - a._id.month)
+                                .map(({ _id, total }, index) => {
+                                    return (
+                                        index < 6 && (
+                                            <tr
+                                                className={s.item}
+                                                key={_id.month}
+                                            >
+                                                <td className={s.description}>
+                                                    {months[_id.month]}
+                                                </td>
+                                                <td className={s.description}>
+                                                    {total}
+                                                </td>
+                                            </tr>
+                                        )
+                                    );
+                                })}
+                    </tbody>
+                </>
+            </table>
+        </div>
+    );
 };
 
 export default Summary;
