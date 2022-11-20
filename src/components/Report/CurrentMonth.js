e0d326455446331cfbb02acda1503886014ab54f
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-20 14:34:40 +0200

    refactoring

--- src/components/Report/CurrentMonth.js
+++ src/components/Report/CurrentMonth.js
@@ -1,40 +1,36 @@
-import React from 'react';
-
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-
-import s from './Report.module.css';
-
-import months from '../../data/months.json';
+import React from "react";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import months from "../../data/months.json";
+import s from "./Report.module.css";
 
 const CurrentMonth = ({
-  currentMonth,
-  currentYear,
-  onHandleClickRight,
-  onHandleClickLeft,
+    currentMonth,
+    currentYear,
+    onHandleClickRight,
+    onHandleClickLeft,
 }) => {
-  const monthToString = String(currentMonth);
-  const selectMonth = months.filter(el => el.id === monthToString);
-  return (
-    <div className={s.reportMonth}>
-      <p className={s.title}>Current period:</p>
-      <div className={s.transactionWrapper}>
-        <ArrowBackIosIcon
-          style={{ color: '#FF751D', width: '12' }}
-          onClick={onHandleClickLeft}
-        />
-        {
-          <span
-            className={s.reportMonthTitle}
-          >{`${selectMonth[0].name} ${currentYear}`}</span>
-        }
-
-        <ArrowForwardIosIcon
-          style={{ color: '#FF751D', width: '12' }}
-          onClick={onHandleClickRight}
-        />
-      </div>
-    </div>
-  );
+    const monthToString = String(currentMonth);
+    const selectMonth = months.filter((el) => el.id === monthToString);
+    return (
+        <div className={s.reportMonth}>
+            <p className={s.title}>Current period:</p>
+            <div className={s.transactionWrapper}>
+                <ArrowBackIosIcon
+                    style={{ color: "#FF751D", width: "12" }}
+                    onClick={onHandleClickLeft}
+                />
+                {
+                    <span
+                        className={s.reportMonthTitle}
+                    >{`${selectMonth[0].name} ${currentYear}`}</span>
+                }
+                <ArrowForwardIosIcon
+                    style={{ color: "#FF751D", width: "12" }}
+                    onClick={onHandleClickRight}
+                />
+            </div>
+        </div>
+    );
 };
 export default CurrentMonth;
