c245de3504d66fe12b55fea6bc85df281fedabcb
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-24 12:12:16 +0200

    update

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,15 +1,14 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
-import { authSelectors } from '../redux/auth';
+import PropTypes from "prop-types";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { authSelectors } from "../redux/auth";
 
-export default function ProtectedRoute({ redirectPath = '/', children }) {
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-
-  return isLoggedIn ? children : <Navigate to={redirectPath} replace />;
+export default function ProtectedRoute({ redirectPath = "/", children }) {
+    const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+    return isLoggedIn ? children : <Navigate to={redirectPath} replace />;
 }
 
 ProtectedRoute.propTypes = {
-  redirectPath: PropTypes.string,
-  children: PropTypes.node,
+    redirectPath: PropTypes.string,
+    children: PropTypes.node,
 };
