4e3755d062ea35850e5866963980b4bff02a51e0
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-25 00:25:00 +0200

    Update

--- src/pages/ReportsView/ReportsView.jsx
+++ src/pages/ReportsView/ReportsView.jsx
@@ -1,98 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import ChartReport from '../../components/ChartReport/ChartReport';
-import { Report } from '../../components/Report';
-import { useSelector, useDispatch } from 'react-redux';
-import s from './ReportView.module.css';
-import Container from '../../components/Container';
-import transactionsSelectors from '../../redux/transactions/transactions-selectors';
-import transactionsOperations from '../../redux/transactions/transactions-operations';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ChartReport from "../../components/ChartReport/ChartReport";
+import Container from "../../components/Container";
+import { Report } from "../../components/Report";
+import transactionsSelectors from "../../redux/transactions/transactions-selectors";
+import transactionsOperations from "../../redux/transactions/transactions-operations";
+import s from "./ReportView.module.css";
+
 const ReportsView = () => {
-  let date = new Date();
-  let selectedMonth = date.getMonth() + 1;
-  let selectedYear = date.getFullYear();
-  const [month, setMonth] = useState(selectedMonth);
-  const [year, setYear] = useState(selectedYear);
-  const [category, setCategory] = useState('');
-  const [title, setTitle] = useState('expense');
-  const [hasChanged, setHasChanged] = useState(false);
-  let monthToString = String(month);
-  let yearToString = String(year);
+    let date = new Date();
+    let selectedMonth = date.getMonth() + 1;
+    let selectedYear = date.getFullYear();
+    let monthToString = String(month);
+    let yearToString = String(year);
+    const [month, setMonth] = useState(selectedMonth);
+    const [year, setYear] = useState(selectedYear);
+    const [category, setCategory] = useState("");
+    const [title, setTitle] = useState("expense");
+    const [hasChanged, setHasChanged] = useState(false);
+    const dispatch = useDispatch();
 
-  const dispatch = useDispatch();
+    const expensesReport = useSelector(
+        transactionsSelectors.getExpencesReportPerMonth,
+    );
+    const incomeReport = useSelector(
+        transactionsSelectors.getIncomeReportPerMonth,
+    );
+    const expenses = useSelector(transactionsSelectors.getExpencesPerMonth);
+    const incomes = useSelector(transactionsSelectors.getIncomePerMonth);
 
-  const expensesReport = useSelector(
-    transactionsSelectors.getExpencesReportPerMonth,
-  );
-  const incomeReport = useSelector(
-    transactionsSelectors.getIncomeReportPerMonth,
-  );
-  const expenses = useSelector(transactionsSelectors.getExpencesPerMonth);
-  const incomes = useSelector(transactionsSelectors.getIncomePerMonth);
+    useEffect(() => {
+        if (monthToString < 10) {
+            dispatch(
+                transactionsOperations.getFullTransactions({
+                    month: `0${monthToString}`,
+                    year: yearToString,
+                }),
+            );
+        } else {
+            dispatch(
+                transactionsOperations.getFullTransactions({
+                    month: monthToString,
+                    year: yearToString,
+                }),
+            );
+        }
+    }, [dispatch, monthToString, yearToString]);
 
-  useEffect(() => {
-    if (monthToString < 10) {
-      dispatch(
-        transactionsOperations.getFullTransactions({
-          month: `0${monthToString}`,
-          year: yearToString,
-        }),
-      );
-    } else {
-      dispatch(
-        transactionsOperations.getFullTransactions({
-          month: monthToString,
-          year: yearToString,
-        }),
-      );
-    }
-  }, [dispatch, monthToString, yearToString]);
+    const onHandleClickRight = () => {
+        if (month < 12) {
+            setMonth((prev) => (prev += 1));
+        } else {
+            setMonth(1);
+            setYear((prev) => (prev += 1));
+        }
+    };
+    const onHandleClickLeft = () => {
+        if (month <= 1) {
+            setMonth(12);
+            setYear((prev) => (prev -= 1));
+        } else {
+            setMonth((prev) => (prev -= 1));
+        }
+    };
 
-  const onHandleClickRight = () => {
-    if (month < 12) {
-      setMonth(prev => (prev += 1));
-    } else {
-      setMonth(1);
-      setYear(prev => (prev += 1));
-    }
-  };
-  const onHandleClickLeft = () => {
-    if (month <= 1) {
-      setMonth(12);
-      setYear(prev => (prev -= 1));
-    } else {
-      setMonth(prev => (prev -= 1));
-    }
-  };
-
-  return (
-    <Container>
-      <div className={s.containerReport}>
-        <Report
-          month={month}
-          year={year}
-          onHandleClickRight={onHandleClickRight}
-          onHandleClickLeft={onHandleClickLeft}
-          setCategory={setCategory}
-          expenses={expenses}
-          incomes={incomes}
-          expensesReport={expensesReport}
-          incomeReport={incomeReport}
-          setTitle={setTitle}
-          setHasChanged={setHasChanged}
-          category={category}
-        />
-        <ChartReport
-          month={month}
-          year={year}
-          category={category}
-          expensesReport={expensesReport}
-          incomeReport={incomeReport}
-          title={title}
-          hasChanged={hasChanged}
-        />
-      </div>
-    </Container>
-  );
+    return (
+        <Container>
+            <div className={s.containerReport}>
+                <Report
+                    month={month}
+                    year={year}
+                    onHandleClickRight={onHandleClickRight}
+                    onHandleClickLeft={onHandleClickLeft}
+                    setCategory={setCategory}
+                    expenses={expenses}
+                    incomes={incomes}
+                    expensesReport={expensesReport}
+                    incomeReport={incomeReport}
+                    setTitle={setTitle}
+                    setHasChanged={setHasChanged}
+                    category={category}
+                />
+                <ChartReport
+                    month={month}
+                    year={year}
+                    category={category}
+                    expensesReport={expensesReport}
+                    incomeReport={incomeReport}
+                    title={title}
+                    hasChanged={hasChanged}
+                />
+            </div>
+        </Container>
+    );
 };
 
 export default ReportsView;
