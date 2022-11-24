aaaa4585684cf1431a4aa82b90a7f5e63ff80c38
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-24 12:09:39 +0200

    update

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,27 +1,26 @@
-// import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
-
+import { useSelector } from "react-redux";
+import authSelectors from "../../redux/auth/auth-selectors";
+import s from "./UserMenu.module.css";
 // import { isDesktop, isTablet, isMobile } from '../../services/mediaQuery';
 // import { Desktop, Tablet, Mobile, Default } from '../../services/mediaQuery';
-import s from './UserMenu.module.css';
-import authSelectors from '../../redux/auth/auth-selectors';
+// import PropTypes from 'prop-types';
 
 const UserMenu = () => {
-  //   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  const email = useSelector(authSelectors.getUserEmail);
-  const googleEmail = useSelector(authSelectors.getUserGoogleEmail);
+    //   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+    const email = useSelector(authSelectors.getUserEmail);
+    const googleEmail = useSelector(authSelectors.getUserGoogleEmail);
 
-  return email ? (
-    <div className={s.userMenu}>
-      <div className={s.username}>{email.split('@')[0][0]}</div>
-      <p className={s.text}>{email.split('@')[0]}</p>
-    </div>
-  ) : googleEmail ? (
-    <div className={s.userMenu}>
-      <div className={s.username}>{googleEmail.split('@')[0][0]}</div>
-      <p className={s.text}>{googleEmail.split('@')[0]}</p>
-    </div>
-  ) : null;
+    return email ? (
+        <div className={s.userMenu}>
+            <div className={s.username}>{email.split("@")[0][0]}</div>
+            <p className={s.text}>{email.split("@")[0]}</p>
+        </div>
+    ) : googleEmail ? (
+        <div className={s.userMenu}>
+            <div className={s.username}>{googleEmail.split("@")[0][0]}</div>
+            <p className={s.text}>{googleEmail.split("@")[0]}</p>
+        </div>
+    ) : null;
 };
 
 export default UserMenu;
