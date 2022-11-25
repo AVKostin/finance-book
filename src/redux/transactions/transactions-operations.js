3727993980c59aa11b62bb3c64324078611b3107
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-26 00:33:32 +0200

    Update

--- src/redux/transactions/transactions-operations.js
+++ src/redux/transactions/transactions-operations.js
@@ -1,93 +1,102 @@
-import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import Notiflix from 'notiflix';
-axios.defaults.baseURL = 'https://finantial-book-kapusta.herokuapp.com/api';
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import Notiflix from "notiflix";
+axios.defaults.baseURL = "https://finantial-book-kapusta.herokuapp.com/api";
 
-const setBalance = createAsyncThunk('/users/balance', async credentials => {
-  try {
-    const { data } = await axios.put('/users/balance', credentials);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+const setBalance = createAsyncThunk("/users/balance", async (credentials) => {
+    try {
+        const { data } = await axios.put("/users/balance", credentials);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-const getTransactions = createAsyncThunk('/transactions', async credentials => {
-  try {
-    const { data } = await axios.get(`/transactions${credentials}`);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-});
+const getTransactions = createAsyncThunk(
+    "/transactions",
+    async (credentials) => {
+        try {
+            const { data } = await axios.get(`/transactions${credentials}`);
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    },
+);
 
-const addTransaction = createAsyncThunk('/transactions', async credentials => {
-  try {
-    const { data } = await axios.post('/transactions', credentials);
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-});
+const addTransaction = createAsyncThunk(
+    "/transactions",
+    async (credentials) => {
+        try {
+            const { data } = await axios.post("/transactions", credentials);
+            console.log(data);
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    },
+);
 
 const deleteTransaction = createAsyncThunk(
-  '/transactions/id',
-  async credentials => {
-    try {
-      const { data } = await axios.delete(
-        `/transactions/${credentials.id}`,
-        credentials,
-      );
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  },
+    "/transactions/id",
+    async (credentials) => {
+        try {
+            const { data } = await axios.delete(
+                `/transactions/${credentials.id}`,
+                credentials,
+            );
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    },
 );
 
 const getTransactionsByType = createAsyncThunk(
-  '/transactions/report/type',
-  async credentials => {
-    try {
-      const { data } = await axios.get(
-        `/transactions/report/${credentials.type}`,
-        credentials,
-      );
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  },
+    "/transactions/report/type",
+    async (credentials) => {
+        try {
+            const { data } = await axios.get(
+                `/transactions/report/${credentials.type}`,
+                credentials,
+            );
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    },
 );
 
 const getFullTransactions = createAsyncThunk(
-  '/transactions/report/full',
-  async (credentials, thunkAPI) => {
-    try {
-      const { data } = await axios.get(
-        `/transactions/report/full?month=${credentials.month}&year=${credentials.year}`,
-        credentials,
-      );
-      if (data.transactions.length === 0) {
-        Notiflix.Notify.warning('You have no transactions for this month ⚠', {
-          timeout: 1500,
-        });
-      }
-      return data;
-    } catch (error) {
-      console.log(error);
-      return thunkAPI.rejectWithValue();
-    }
-  },
+    "/transactions/report/full",
+    async (credentials, thunkAPI) => {
+        try {
+            const { data } = await axios.get(
+                `/transactions/report/full?month=${credentials.month}&year=${credentials.year}`,
+                credentials,
+            );
+            if (data.transactions.length === 0) {
+                Notiflix.Notify.warning(
+                    "You have no transactions for this month ⚠",
+                    {
+                        timeout: 1500,
+                    },
+                );
+            }
+            return data;
+        } catch (error) {
+            console.log(error);
+            return thunkAPI.rejectWithValue();
+        }
+    },
 );
 
 const transactionsOperations = {
-  setBalance,
-  getTransactions,
-  addTransaction,
-  deleteTransaction,
-  getTransactionsByType,
-  getFullTransactions,
+    setBalance,
+    addTransaction,
+    getTransactions,
+    deleteTransaction,
+    getFullTransactions,
+    getTransactionsByType,
 };
 export default transactionsOperations;
