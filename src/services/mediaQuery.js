548fb502043ec198e6fd6fd55cd76cb939fbdd9a
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-27 23:27:19 +0200

    Update

--- src/services/mediaQuery.js
+++ src/services/mediaQuery.js
@@ -1,26 +1,23 @@
-import { useMediaQuery } from 'react-responsive';
+import { useMediaQuery } from "react-responsive";
 
 const logicMediaQuery = (children, device) => {
-  return device ? children : null;
+    return device ? children : null;
 };
 
-export const isDesktop = cb => cb({ minWidth: 1280 });
-export const isTablet = cb => cb({ minWidth: 768, maxWidth: 1279 });
-export const isMobile = cb => cb({ maxWidth: 767 });
-export const isNotMobile = cb => cb({ minWidth: 768 });
+export const isMobile = (cb) => cb({ maxWidth: 767 });
+export const isTablet = (cb) => cb({ minWidth: 768, maxWidth: 1279 });
+export const isDesktop = (cb) => cb({ minWidth: 1280 });
+export const isNotMobile = (cb) => cb({ minWidth: 768 });
 
 export const Desktop = ({ children }) => {
-  return logicMediaQuery(children, isDesktop(useMediaQuery));
+    return logicMediaQuery(children, isDesktop(useMediaQuery));
 };
-
 export const Tablet = ({ children }) => {
-  return logicMediaQuery(children, isTablet(useMediaQuery));
+    return logicMediaQuery(children, isTablet(useMediaQuery));
 };
-
 export const Mobile = ({ children }) => {
-  return logicMediaQuery(children, isMobile(useMediaQuery));
+    return logicMediaQuery(children, isMobile(useMediaQuery));
 };
-
 export const Default = ({ children }) => {
-  return logicMediaQuery(children, isNotMobile(useMediaQuery));
+    return logicMediaQuery(children, isNotMobile(useMediaQuery));
 };
